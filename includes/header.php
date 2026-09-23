ab8fc13be8d53da0ff6b7f18d9782ad109973944
Eduardo-Nicolete37 <eduardonicolete37@gmail.com>
2026-09-23 14:34:09 -0300

    feat: estilização da navbar

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,21 @@
 <header>
     <nav class="navbar">
+        <div class="navbar1">
         <a href="../">Inicio</a>
         <a href="../app/create.php">Cadastrar</a>
         <a href="../app/read.php">Vizualizar todos</a>
         <a href="../app/readWithWhere.php">Vizualizar um</a>
         <a href="../app/update.php">Atualizar registro</a>
         <a href="../app/delete.php">Deletar Registro</a>
+        </div>
+        <div class="navbar2">
+        <a href="../login/login.php">Login</a>
+        <?php
+        if (isset($_SESSION['id'])) {
+            echo "<a href='../login/logout.php'>Logout</a>";
+        }
+        ?>
+        </div>
     </nav>
     <hr>
 </header>
